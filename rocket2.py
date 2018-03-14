62ff1323add71ad18593f2cf52d0b004ad34a6db
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2018-03-14 10:29:49 -0400

    Updated from Brython Server: 3/14/2018 10:29:48 AM

--- rocket2.py
+++ rocket2.py
@@ -10,5 +10,5 @@ G = 6.674E-11
 Ve=sqrt(2*Me*G/Re)
 print("Predicted escape velocity is ", Ve, " m/s")
 
-rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=10)
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve)
 earth.run(rocket)
